ccc55e437aaa454d7c8b8b06e4e08e93327ba466
user <user@email.com>
2024-04-18 15:48:54 -0400

    fith commit

--- restaurant/models.py
+++ restaurant/models.py
@@ -8,6 +8,9 @@ class Menu(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField(default=5)
 
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
+
 
 class Booking(models.Model):
     id = models.IntegerField(primary_key=True)
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,19 +1,26 @@
+from django.shortcuts import render
 from rest_framework.decorators import api_view
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 from rest_framework import generics, viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
 
 # Create your views here. 
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     permission_classes = [IsAuthenticated] 
+
+def index(request):
+    return render(request, 'index.html', {})
